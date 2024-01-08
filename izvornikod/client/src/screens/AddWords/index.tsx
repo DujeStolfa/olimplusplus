3e304d6975350e07747fb66d628ff65b17c15a0e
Gabrijel-Cobanov <gabrijelcobanov@gmail.com>
2024-01-08 20:16:26 +0100

    Dodao početke funkcionalnosti za screen AddWords
    Vučem iz Statea words, te ih isprintam u DataGrid, dodao sam temp_rows samo da ne moram dodavati riječi u bazu sada, to ćemo kasnije skloniti

--- izvornikod/client/src/screens/AddWords/index.tsx
+++ izvornikod/client/src/screens/AddWords/index.tsx
@@ -1,20 +1,49 @@
-import React, { useEffect } from "react";
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
-import { Button, Container, Stack, Typography, Box} from "@mui/material"; // Yoink Dujinog koda i importa iz Logina
+import React, { useEffect, useState } from "react";
+import { DataGrid, GridColDef, GridRowId, GridRowSelectionModel } from '@mui/x-data-grid';
+import { Button, Container, Stack, Typography, Box} from "@mui/material"; 
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
 
-var columns: GridColDef[] = [
-    {field: "id", headerName: "ID"},
-    {field: "word", headerName: "Word"},
-    {field: "translation", headerName: "Translation"}
-]
 
-var rows = [
-    {id: 1, word: "Desk", translation: "Stol"},
-    {id: 2,word: "Edge", translation: "Ivica"},
-    {id: 3,word: "Pot", translation: "Marica"}
-]
+
 
 const AddWords = () => {
+
+    const [selectedWords, setSelectedWords] = useState<any[]>([]);
+    const { words } = useSelector((state: RootState) => state.words); // Ovo je nadam se tocno
+
+    var columns: GridColDef[] = [
+        {field: "id", headerName: "ID", flex: 1},
+        {field: "word", headerName: "Word", flex: 1},
+        {field: "translation", headerName: "Translation", flex: 1}
+    ]
+    
+    var rows = words.map(el =>({
+        id: el.wordid,
+        word: el.foreignname,
+        translation: el.croatianname
+    }))
+
+    var temp_rows = [
+        {id: 34, word: "table", translation: "stol"},
+        {id: 35, word: "edge", translation: "ivica"},
+        {id: 36, word: "pot", translation: "marica"}
+    ]   // Ovo cemo kasnije skloniti
+
+    temp_rows.forEach(el => {rows.push(el)})
+
+    const handleRowSelection = (newSelection: GridRowSelectionModel) => {
+        const selectedRows = newSelection as GridRowId[];
+        const selectedData = selectedRows.map((rowId) => {
+        const selectedRow = rows.find((row) => row.id === rowId);
+        return selectedRow ? { id: selectedRow.id, word: selectedRow.word, translation: selectedRow.translation } : null;
+        });
+        const filteredSelectedData = selectedData.filter((row) => row !== null);
+        console.log(selectedWords)      
+        // Trenutno ima glitch gdje ne azurira odma kako treba, al cijeli dan
+        // ovo grindam, za sada je dobro
+        setSelectedWords(filteredSelectedData);
+    };
 
     return (
     
@@ -28,13 +57,20 @@ const AddWords = () => {
                     initialState={{
                     pagination: {
                         paginationModel: {
-                        pageSize: 5,
+                            pageSize: 5,
                         },
                     },
                     }}
                     pageSizeOptions={[5]}
                     checkboxSelection
                     disableRowSelectionOnClick
+                    sx = {{
+                        marginTop: "1em",
+                        borderColor: "black"
+                    }}
+                    onRowSelectionModelChange={(newSelectionModel) => {
+                        handleRowSelection(newSelectionModel);
+                      }}
                 />
 
                 <Box
